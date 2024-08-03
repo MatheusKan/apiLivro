d0b7caeac918dcbf37fa01743f6b753396291fa6
MatheusKan <matheuskan@hotmail.com>
2024-08-02 22:48:29 -0300

    V2.0

---
+++ app/Http/Controllers/TbLivrosController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TbLivrosController extends Controller
+{
+    //contruir o crud
+    // Crud -> Read(leitura) Select
+
+    //Mostrar os registros da tabela tb_livros
+    public function index(){
+
+    }
+    //Mostrar um tipo de registro
+    //Crud -> Read(leitura) Select
+    public function show(){
+    }
+    //Cadastrar registros
+    //Crud -> Create(criar/cadastrar) 
+    public function store(){
+    }
+    //Alterar registros
+    //Crud -> Update(alterar) 
+    public function update(){
+    }
+    //Deletar registros
+    //Crud -> Delete(criar/cadastrar)
+    public function destroy(){
+    }
+
+
+
+}
